24fd55624ab88996278489fc17c4f4ea65502fa3
KimHyunse <anzmf12@naver.com>
2018-07-01 23:19:27 -0400

    Add Color Function

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -2,87 +2,107 @@ import React, { Component } from 'react';
 import TodoTemplate from './components/TodoTemplate';
 import Form from './components/Form';
 import TodoItemList from './components/TodoItemList';
-
+import Palette from './components/Palette';
 
 class App extends Component {
-  id = 3 // 이미 0,1,2 가 존재하므로 3으로 설정
+  id = 3; // 이미 0,1,2 가 존재하므로 3으로 설정
 
   state = {
     input: '',
     todos: [
-      { id: 0, text: 'HI', checked: false },
-      { id: 1, text: 'React', checked: true },
-      { id: 2, text: 'Haha', checked: false }
-    ]
+      { id: 0, text: 'HI', checked: false, color: '#343a40' },
+      { id: 1, text: 'React', checked: true, color: '#343a40' },
+      { id: 2, text: 'Haha', checked: false, color: '#343a40' }
+    ],
+    color: ''
   };
 
   handleChange = (e) => {
     this.setState({
       input: e.target.value
-    }); 
-  }
+    });
+  };
 
   handleCreate = () => {
-    const { input, todos } = this.state;
+    const { input, todos, color } = this.state;
     this.setState({
       input: '',
       todos: todos.concat({
         id: this.id++,
         text: input,
-        checked: false
+        checked: false,
+        color: color
       })
     });
-  }
+  };
 
   handleKeyPress = (e) => {
-    if(e.key === 'Enter') {
+    if (e.key === 'Enter') {
       this.handleCreate();
     }
-  }
+  };
 
   handleToggle = (id) => {
     const { todos } = this.state;
-    const index = todos.findIndex(todo => todo.id === id);
+    const index = todos.findIndex((todo) => todo.id === id);
     const selected = todos[index];
     const nextTodos = [...todos];
 
     nextTodos[index] = {
       ...selected,
       checked: !selected.checked
-    }
+    };
 
     this.setState({
       todos: nextTodos
     });
-  }
+  };
 
   handleRemove = (id) => {
     const { todos } = this.state;
     this.setState({
-      todos: todos.filter(todo => todo.id !== id)
+      todos: todos.filter((todo) => todo.id !== id)
     });
-  }
+  };
 
+  selectColor = (color) => {
+    this.setState({
+      color
+    });
+  };
 
   render() {
-    const { input, todos } = this.state;
+    const { input, todos, color } = this.state;
     const {
       handleChange,
       handleCreate,
       handleKeyPress,
       handleToggle,
-      handleRemove
+      handleRemove,
+      selectColor
     } = this;
+    const paletteColorArr = ['#343a40', '#f03e3e', '#12b886', '#228ae6'];
 
     return (
-      <TodoTemplate form={(
-        <Form 
-          value={input}
-          onKeyPress={handleKeyPress}
-          onChange={handleChange}
-          onCreate={handleCreate}
-        />)}>
-        <TodoItemList todos={todos} onToggle={handleToggle} onRemove={handleRemove}/>
+      <TodoTemplate
+        form={
+          <Form
+            value={input}
+            onKeyPress={handleKeyPress}
+            onChange={handleChange}
+            onCreate={handleCreate}
+          />
+        }
+        palette={
+          <Palette colors={paletteColorArr} onSelectColor={selectColor} />
+        }
+      >
+        <TodoItemList
+          todos={todos}
+          onToggle={handleToggle}
+          onRemove={handleRemove}
+          color={color}
+        />
       </TodoTemplate>
     );
   }
