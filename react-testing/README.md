750286d9c3412ff2daf47c8f6b0c4287ff42a473
KimHyunse <anzmf12@naver.com>
2018-07-03 19:26:16 -0400

    Apply Absoulte Route Path / CommentBox Testing

--- react-testing/README.md
+++ react-testing/README.md
@@ -1,2 +1,3 @@
 # React-Testing
 - To learn how to test codes that I write in React.
+- Jest & Enzyme
--- react-testing/src/components/CommentBox.js
+++ react-testing/src/components/CommentBox.js
@@ -1,11 +1,31 @@
 import React, { Component } from 'react';
 
 class CommentBox extends Component {
+  state = { comment: '' };
+
+  handleChange = (e) => {
+    this.setState({ comment: e.target.value });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Call an Event Creator
+
+    // Save a comment
+
+    this.setState({ comment: e.target.value });
+  };
+
   render() {
     return (
-      <div>
-        CommentBox Area
-      </div>
+      <form onSubmit={this.handleSubmit}>
+        <h4>CommentBox Area</h4>
+        <textarea value={this.state.comment} onChange={this.handleChange} />
+        <div>
+          <button>Submit Comment</button>
+        </div>
+      </form>
     );
   }
 }
